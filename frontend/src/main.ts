f153c80fffa87847f84dd11eaca84f6b31f3efa3
Sergey Kosykh <s3rkosal@gmail.com>
2025-11-24 22:34:18 +0400

    added basic editor

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,16 +2,33 @@ import './style.css'
 
 import { setupCounter } from './counter.ts'
 
+import EditorJS from '@editorjs/editorjs';
+
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Vite + TypeScript</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <div id="editorjs">
+    </div>
   </div>
 `
+const editor = new EditorJS({
+
+  /**
+   * Id of Element that should contain Editor instance
+   */
+  holder: 'editorjs'
+});
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+try {
+  await editor.isReady;
+  console.log('Editor.js is ready to work!')
+  /** Do anything you need after editor initialization */
+} catch (reason) {
+  console.log(`Editor.js initialization failed because of ${reason}`)
+}
