08d0a8d324ea5293c6a20d800a5ee071de028dfe
Sergey Kosykh <s3rkosal@gmail.com>
2025-11-25 16:41:49 +0400

    improved django views using templates

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, detail, edit
 
+app_name = "blog"
 urlpatterns = [
-    path('', index, name="index")
+    path('',                 index,  name="index"),
+    path('<int:post_id>/',   detail, name="detail"),
+    path('<int:post_id>/e/', edit,   name="edit"), 
 ] 
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,7 +1,41 @@
 #from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
 
 # Create your views here.
 
 def index(req: HttpRequest) -> HttpResponse:
-    return HttpResponse("blog index")
+    
+    latest_posts = Post.objects.order_by("-published_at")[:5]
+    
+    context = {
+        "latest_posts": latest_posts,
+        "title": "Latest posts"
+    }
+    
+    return render(req, "blog/index.html", context=context)
+
+
+def detail(req: HttpRequest, post_id: int) -> HttpResponse:
+    
+    post = get_object_or_404(Post, pk=post_id)
+    
+    context = {
+        "post": post,
+        "title": post.title
+    }
+    
+    return render(req, "blog/detail.html", context=context)
+
+
+def edit(req: HttpRequest, post_id: int) -> HttpRequest:
+    post = get_object_or_404(Post, pk=post_id)
+    
+    context = {
+        "post": post,
+        "title": f"editing {post.title}"
+    }
+    
+    return render(req, "blog/edit.html", context=context)
