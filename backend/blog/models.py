72bf24af4e5b059ec196ecc61c24ba1ea9cfc40d
Sergey Kosykh <s3rkosal@gmail.com>
2025-11-25 15:18:12 +0400

    added basic django model for posts

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+# from django.contrib.auth.base_user import AbstractBaseUser
+
 # Create your models here.
+class Post(models.Model):
+    
+    # author field
+    # author = AbstractBaseUser
+    
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    
+    started_at = models.DateTimeField()
+    last_edited = models.DateTimeField()
+    published_at = models.DateTimeField()
+    
+    is_published = models.BooleanField() 
