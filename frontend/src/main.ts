87975ac8e6471abc91426d80ea626f08f74393c4
Sergey Kosykh <s3rkosal@gmail.com>
2025-11-25 14:51:26 +0400

    added more editor.js tools + config

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,13 @@ import { setupCounter } from './counter.ts'
 
 import EditorJS from '@editorjs/editorjs';
 
+import type { BlockToolConstructable } from '@editorjs/editorjs';
+
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
+import Quote from '@editorjs/quote';
+import Warning from '@editorjs/warning'
+import Paragraph from '@editorjs/paragraph';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -20,7 +27,36 @@ const editor = new EditorJS({
   /**
    * Id of Element that should contain Editor instance
    */
-  holder: 'editorjs'
+  holder: 'editorjs',
+  autofocus: true,
+  placeholder: 'Let`s write an awesome story!',
+
+  tools: { 
+    header: {
+      class: Header as unknown as BlockToolConstructable, 
+      inlineToolbar: true 
+    }, 
+    list: { 
+      class: List, 
+      inlineToolbar: true 
+    },
+    quote: {
+      class: Quote,
+      inlineToolbar: true
+    },
+    warning: {
+      class: Warning,
+      inlineToolbar: true,
+      config: {
+        titlePlaceholder: 'Warning title',
+        messagePlaceholder: 'warning message warning',
+      },
+    },
+    paragraph: {
+      class: Paragraph as unknown as BlockToolConstructable,
+      inlineToolbar: true
+    }
+  }, 
 });
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
