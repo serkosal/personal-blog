522df48c932de68e24affb1cf69b4df7f8294d59
Sergey Kosykh <s3rkosal@gmail.com>
2025-11-29 21:23:55 +0400

    added basic register functionallity

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from .views import profile
+from .views import profile, RegisterView
 
 app_name = "users"
 urlpatterns = [
     
     path('', include("django.contrib.auth.urls")),
     path('profile/', profile, name='profile'),
+    path('register/', RegisterView.as_view(), name="register")
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,16 @@
-from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('users:login')
+    template_name = "registration/register.html"
+
 
 def profile(req: HttpRequest) -> HttpResponse:
 
