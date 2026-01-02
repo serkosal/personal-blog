5184576d2d3e934be4a71647ed3a28020d0535a2
Sergey Kosykh <s3rkosal@gmail.com>
2026-01-02 23:29:12 +0400

    try logging propagation

--- backend/src/main/settings.py
+++ backend/src/main/settings.py
@@ -88,6 +88,28 @@ TEMPLATES = [
 WSGI_APPLICATION = 'main.wsgi.application'
 
 
+# logging
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
